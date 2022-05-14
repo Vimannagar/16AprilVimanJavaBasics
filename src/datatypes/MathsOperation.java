095080117d133572347c4c41ba69ea1c96571c0d
Gourav Jawale <velocity.ctc@gmail.com>
2022-05-14 10:17:42 +0530

    Non Primitive data type

--- src/datatypes/MathsOperation.java
+++ src/datatypes/MathsOperation.java
@@ -8,12 +8,12 @@ public class MathsOperation {
 // Addition		
 	int	val3 =  val2 + val1;
 	
-	System.out.println(val3);
+	System.out.println(val3);//70
 	
 //	Substraction
 	int val4= val2- val1;
 	
-	System.out.println(val4);
+	System.out.println(val4);//-30
 	
 //	Multiplication
 	
@@ -22,20 +22,20 @@ public class MathsOperation {
 	
 	int k = i*j;
 	
-	System.out.println(k);
+	System.out.println(k);//200
 
 //	Division
 	
 	
 	double val5 = val1/val2;
 	
-	System.out.println(val5);
+	System.out.println(val5);//2.0
 	
 	double val6 = 50;
 	double val7 = 20;
 	double val8 = val6/val7;
 	
-	System.out.println(val8);
+	System.out.println(val8);//2.5
 	
 	
 		
---
+++ src/datatypes/NonPrimitiveDataType.java
@@ -0,0 +1,36 @@
+package datatypes;
+
+public class NonPrimitiveDataType {
+
+	
+	public static void main(String[] args) {
+		String s = "This is String value";
+		
+		System.out.println(s);//This is String value
+		
+		
+		System.out.println("String"+" "+"value");
+
+		// + is known as concatenation operator when it is used for String. Which is used to join the values
+		
+		
+	
+		
+		System.out.println("Statement :"+s);//Statement :This is String value
+		
+		String s1 = "abc";
+		String s2 = "def";
+		System.out.println(s1+s2);//abcdef
+		
+		System.out.println(s1+50);//abc50
+		
+		System.out.println(s2+2+2);//def22
+		
+		System.out.println(2+2+s2);//4def
+		
+		
+		
+		
+		
+	}
+}
