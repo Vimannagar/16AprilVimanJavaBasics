b701ade2dced8bbae09e574909686e3410bc412d
Gourav Jawale <gouravjawale28@gmail.com>
2022-08-13 08:31:51 +0530

    Array programs

---
+++ src/arraysdiscussion/FindCommoDigit.java
@@ -0,0 +1,21 @@
+package arraysdiscussion;
+
+public class FindCommoDigit {
+	
+	public static void main(String[] args) {
+		int [] arr1 = {1, 5, 8, 45, 60};
+		
+		int [] arr2 = {7, 6, 1, 45, 60};
+		
+		int [] arr3 = {1, 60, 5, 80, 7};
+		
+//		find out the common digit from all 3 array
+		
+		
+		
+		int []a1 = {1, 8, 2, 4, 6};
+		
+//		WAP to find the triplets / subarray which makes the sum=11
+	}
+
+}
